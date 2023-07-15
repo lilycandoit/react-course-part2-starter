1ddb84c0918978fe49507484e5bea6af916051a6
lilycandoit <hueduong288@gmail.com>
2023-07-15 19:24:48 +0800

    parameterized queries

---
+++ src/routing/hooks/usePosts.ts
@@ -0,0 +1,25 @@
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+
+interface Post {
+  id: number;
+  title: string;
+  body: string;
+  userId: number;
+}
+
+const usePosts = (userId: number | undefined) =>
+  useQuery<Post[], Error>({
+    queryKey: userId ? ['users', userId, 'post'] : ['post'],
+    queryFn: () =>
+      axios
+        .get('https://jsonplaceholder.typicode.com/posts', {
+          params: {
+            userId,
+          },
+        })
+        .then((res) => res.data),
+    staleTime: 1 * 60 * 1000, //1m
+  });
+
+export default usePosts;
