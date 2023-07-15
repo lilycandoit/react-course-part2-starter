1c6fe1267e81a6bc595922f2851a91390602e5ed
lilycandoit <hueduong288@gmail.com>
2023-07-15 19:45:28 +0800

    Previous and next buttons

--- src/routing/hooks/usePosts.ts
+++ src/routing/hooks/usePosts.ts
@@ -8,18 +8,25 @@ interface Post {
   userId: number;
 }
 
-const usePosts = (userId: number | undefined) =>
+interface PostQuery {
+  page: number;
+  pageSize: number;
+}
+
+const usePosts = (query: PostQuery) =>
   useQuery<Post[], Error>({
-    queryKey: userId ? ['users', userId, 'post'] : ['post'],
+    queryKey: ['post', query],
     queryFn: () =>
       axios
         .get('https://jsonplaceholder.typicode.com/posts', {
           params: {
-            userId,
+            _start: (query.page-1) * query.pageSize,
+            _limit: query.pageSize,
           },
         })
         .then((res) => res.data),
     staleTime: 1 * 60 * 1000, //1m
+    keepPreviousData: true, // to keep the page not jumping up and down while loading data to next page
   });
 
 export default usePosts;
