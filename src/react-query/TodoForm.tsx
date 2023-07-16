c021d92f5524c12702811a2e91d79a786e5dffde
lilycandoit <hueduong288@gmail.com>
2023-07-16 22:33:07 +0800

    Optimistic updates_still errrors

--- src/react-query/TodoForm.tsx
+++ src/react-query/TodoForm.tsx
@@ -3,29 +3,44 @@ import { useRef } from 'react';
 import { Todo } from '../routing/hooks/useTodos';
 import axios from 'axios';
 
+interface AddTodoContext {
+  previousTodos: Todo [];
+}
+
 const TodoForm = () => {
   const queryClient = useQueryClient();
 
-  const addTodo = useMutation<Todo, Error, Todo>({
+  const addTodo = useMutation<Todo, Error, Todo, AddTodoContext>({
     mutationFn: (todo: Todo) =>
       axios
         .post<Todo>('https://jsonplaceholder.typicode.com/todos', todo)
         .then((res) => res.data),
 
+    onMutate: (newTodo: Todo) => {
+      const previousTodos = queryClient.getQueryData<Todo []>(['todos'] || []);
+
+      queryClient.setQueryData<Todo[]>(['todos'], (todos) => [
+        newTodo, ...(todos || []),
+      ]);
+      if (ref.current) ref.current.value = ''; // to clear input of form
+
+      return {previousTodos};
+    },
+
     //onSuccess, onError, onSettled...
     onSuccess: (savedTodo, newTodo) => {
-      // approach `: invalidating the cache
-      // queryClient.invalidateQueries({
-      //   queryKey: ['todo']// but it does not work with JSONplaceholder
-      // })
-
-      //APPROACH 2:  Updating the data in the cache directly
-      queryClient.setQueryData<Todo[]>(['todos'], (todos) => [
-        savedTodo,
-        ...(todos || []),
-      ]);
-      if (ref.current) ref.current.value=''; // to clear input of form
+      queryClient.setQueryData<Todo[]>(['todos'], (todos) =>
+        todos?.map((todo) => 
+        todo === newTodo ? savedTodo : todo
+        )
+      );
     },
+
+    onError: (error, newTodo, context) => {
+      if (!context) return;
+
+      queryClient.setQueryData<Todo []>(['todos'], context.previousTodos);
+    }
   });
   const ref = useRef<HTMLInputElement>(null);
 
