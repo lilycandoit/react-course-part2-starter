72f89bb5f66fc2acedb82b61ffb35b83d46d4271
lilycandoit <hueduong288@gmail.com>
2023-08-19 23:42:37 +0800

    Preventing unnescessary Renders with Selectors

--- src/state-management/NavBar.tsx
+++ src/state-management/NavBar.tsx
@@ -5,7 +5,11 @@ import useCounterStore from './counter/store';
 
 const NavBar = () => {
   const { tasks } = useContext(TasksContext);
-  const { counter } = useCounterStore();
+  const counter = useCounterStore(s => s.counter);//using arrow function to choose that we want the state change only at counter component, the reset would not be affected
+  //we get the actual counter property  
+
+console.log('render navbar');
+
   return (
     <nav className="navbar d-flex justify-content-between">
       <span className="badge text-bg-secondary">
--- src/state-management/counter/store.ts
+++ src/state-management/counter/store.ts
@@ -2,14 +2,16 @@ import { create } from "zustand";
 
 interface CounterStore {
   counter: number;
+  max: number;
   increment: () => void;
   reset: () => void;
 }
 
 const useCounterStore = create<CounterStore>(set => ({
   counter: 0,
+  max: 5,
   increment: () => set(store => ({ counter: store.counter + 1})), // 1 arrow function with no parameter, using Set that take current state "store", and return the next state
-  reset: () => set (() => ({ counter: 0})), // we havent use the store parameter, so we can use the empty (). 
+  reset: () => set (() => ({ max: 10})), // we havent use the store parameter, so we can use the empty (). 
 }));
 
 export default useCounterStore;
