ad397a4846de334ee8081b2fb59d52c7dbc12fbb
lilycandoit <hueduong288@gmail.com>
2023-08-22 22:33:32 +0800

    Layout Routes

--- src/routing/routes.tsx
+++ src/routing/routes.tsx
@@ -1,19 +1,28 @@
 import { createBrowserRouter } from 'react-router-dom';
+import ErrorPage from './ErrorPage';
 import HomePage from './HomePage';
 import Layout from './Layout';
+import PrivateRoute from './PrivateRoute';
 import UserDetail from './UserDetail';
 import UsersPage from './UsersPage';
-import ErrorPage from './ErrorPage';
+import LoginPage from './LoginPage';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Layout />,
-    errorElement: <ErrorPage  />,
+    errorElement: <ErrorPage />,
     children: [
       { path: '/', element: <HomePage /> }, //same:  { index: true, element: <HomePage /> }
+      { path: '/login', element: <LoginPage  />}
+    ],
+  },
+  //below is call the layout route
+  {
+    element: <PrivateRoute />,
+    children: [
       {
-        path: '/users',
+        path: 'users',
         element: <UsersPage />,
         children: [{ path: ':id', element: <UserDetail /> }],
       },
